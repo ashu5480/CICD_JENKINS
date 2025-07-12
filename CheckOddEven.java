2e8cda4dda1754762232ec5fb8927ca809c85437
ashutosh -singh <singhashu772@gmail.com>
2025-07-12 10:50:53 +0530

    this is first commit for check even odd

---
+++ CheckOddEven.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class CheckOddEven {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int num = sc.nextInt();
+		System.out.println("Enter your Number : "+num);
+		if(num%2==0) {
+			System.out.println("GIven Number is Even");
+		}
+		else {
+			System.out.println("Given Number is Odd");
+		}
+	}
+}
