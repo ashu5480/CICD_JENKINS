28228a968dfc37dfd03d9727f744cc882d4947a1
ashutosh -singh <singhashu772@gmail.com>
2025-07-12 13:57:18 +0530

    this is first for students

--- student.java
+++ student.java
@@ -3,5 +3,6 @@ public class student {
 
 	public static void main(String[] args) {
 		System.out.println("this is first student named");
+		System.out.println("Check whether it is working or not");
 	}
 }
